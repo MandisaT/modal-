1b6e0f222e6e89d9560346236701d4d511caba5a
MandisaT <mandisa.thomas1@gmail.com>
2016-04-15 12:35:01 -0400

    initial commit

---
+++ javascript/script.js
@@ -0,0 +1,45 @@
+
+$(document).ready(function(){
+
+
+// $(".button2").click(function(){
+		$(".box2").hide();
+	// });
+
+	$(".button").click(function(){
+		$(".opbackground").show();
+	});
+	// $('.opbackground').click(function(){
+	// 	$(this).show();
+	// });
+	
+	// $('box2').click(function(){
+	// 	$(this).show();
+	// });
+
+
+
+$(" .opbackground"). on('click',function(){
+	$(".wrapper").hide();
+	 });
+
+
+$(".wrapper").click(function(){
+		$(".box2").show();
+	 });
+
+$(".button2").click(function(){
+		$(".box2").show();
+	 });
+	// $("img").click(function(){
+	// 	$(".opbackground").hide();
+
+
+	// });
+
+
+	// $('box2').click(function(){
+	// 	$(this).hide();
+	// });
+	
+});
